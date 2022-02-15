3d53d9acee44fc1e0bd9221ad4155581736f397d
森田健太 <k_morita@arsaga.jp>
2022-02-15 19:37:26 +0900

    feat: add device ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get "/test", to: "campaigns#test"
   root to: "campaigns#index"
   resources :campaigns, except: [:edit, :update, :destroy] do
